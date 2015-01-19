5a055e936db9ee5777b6af0f45b25df4252e8ee9
arun-r80 <arun_r80@yahoo.co.in>
2015-01-19 09:36:12 +0530

    Custom directive - With errors
    Custom directive - with errors in accessing parent scope within
    directive

--- www/js/controllers/mycontrollers.js
+++ www/js/controllers/mycontrollers.js
@@ -8,12 +8,47 @@ var mymodule = angular.module('mymodule', []);
 mymodule.controller('mycontroller',['$scope','$window','saveContacts',function($scope,$window,saveContacts){
   //define greeting message;
   $scope.contact = {};
+  
   /*Create save function to test data save*/
   $scope.save = function (contact){
     saveContacts(contact);
   };
   
 }]);
+
+var AR_NAME_REGEXP = /[a-z]/i;
+
+mymodule.directive('arname',  function(){
+  // Runs during compile
+  return {
+    // name: '',
+    // priority: 1,
+    // terminal: true,
+    scope: {ardirective:'=ngModel'},
+    //{}, // {} = isolate, true = child, false/undefined = no change
+    // controller: function($scope, $element, $attrs, $transclude) {},
+    require: 'ngModel',
+    // Array = multiple requires, ? = optional, ^ = check parent elements
+    restrict: 'AEC', 
+    // E = Element, A = Attribute, C = Class, M = Comment
+    // template: '',
+    // templateUrl: '',
+    // replace: true,
+    // transclude: true,
+    // compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
+    link: function($scope, iElm, iAttrs, ctrl) {
+      
+
+        ctrl.$validators.arname = function(modelvalue, viewvalue){
+          if(ctrl.$isEmpty(modelvalue)) {return true;}
+          
+          if (AR_NAME_REGEXP.test(viewvalue) ) {return true;}
+          return false;
+        };
+    
+  }
+};
+});
 
 mymodule.factory('saveContacts', ['$window', function($window){
   
